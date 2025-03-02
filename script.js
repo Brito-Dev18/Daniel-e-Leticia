b1920f6c25f3be59d8d30b8037754b0e3252598e
Brito-Dev18 <quilionmimizento@gmail.com>
2025-03-02 15:09:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function updateTime() {
     const months = Math.floor((diff % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30));
     const days = Math.floor((diff % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
     const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const minutes = Math.floor((diff % (1000 * 60)) / (1000 * 60));
     const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
     document.getElementById('years').innerText = `${years} anos`;
